8298dbd049afacf876e4cc8e097d8a62ceac815f
crixxcroxx <cristajesawa@gmail.com>
2021-10-24 08:01:13 +0800

    Revert "non-async version"
    This reverts commit 3bac4f96aa0e665dcf215eb3c8cdacb5a68fa98c.

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-/* (async function f() {
+(async function f() {
   let data = await ( await fetch(`./data.json`).catch(err => console.error(err))).json()
   let idx = Math.floor(Math.random() * data.students.length)
 
   document.getElementById(`name`).innerText = data.students[idx]
-})() */
-
-fetch(`./data.json`)
-  .then(res => {
-    res.json()
-  })
-  .then(d => {
-    let idx = Math.floor(Math.random() * data.students.length)
-
-    document.getElementById(`name`).innerText = data.students[idx]
-  })
-  .catch(err => console.error(err))
+})()
