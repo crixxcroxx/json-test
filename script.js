d0ec48c04c0e1ae763f9b59faa89d5a6047000c3
crixxcroxx <cristajesawa@gmail.com>
2021-10-24 08:09:52 +0800

    non-async

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-(async function f() {
-  let data = await ( await fetch(`./data.json`).catch(err => console.error(err))).json()
+// (async function f() {
+//   let data = await ( await fetch(`./data.json`).catch(err => console.error(err))).json()
+//   let idx = Math.floor(Math.random() * data.students.length)
+//
+//   document.getElementById(`name`).innerText = data.students[idx]
+// })()
+
+fetch(`./data.json`)
+.then(res => res.json())
+.then(data => {
   let idx = Math.floor(Math.random() * data.students.length)
 
   document.getElementById(`name`).innerText = data.students[idx]
-})()
+  })
